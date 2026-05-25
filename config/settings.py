5cee8017ea31dcc7c4a330533d90fd565cff0280
Bhargavi Kambam <bhargavikambam32@gmail.com>
2026-05-25 20:20:24 +0530

    fixed render email env

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,12 +130,12 @@ STATICFILES_DIRS = [
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = True
-EMAIL_TIMEOUT = 5
-EMAIL_HOST_USER = 'hanudigitalhub@gmail.com'
-EMAIL_HOST_PASSWORD = 'purzysgsjhrnnjgh'
+EMAIL_TIMEOUT = 10
+EMAIL_HOST_USER =  os.environ.get('hanudigitalhub@gmail.com')
+EMAIL_HOST_PASSWORD =  os.environ.get('purzysgsjhrnnjgh')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
