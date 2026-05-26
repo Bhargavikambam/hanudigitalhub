4571cb72ca304472fa918e78b4e8d89b0a408ca8
Bhargavi Kambam <bhargavikambam32@gmail.com>
2026-05-26 11:22:30 +0530

    fixed render email env

--- config/settings.py
+++ config/settings.py
@@ -129,21 +129,19 @@ STATICFILES_DIRS = [
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
 
-EMAIL_PORT = 587
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 
 EMAIL_USE_TLS = True
 
 EMAIL_TIMEOUT = 10
 
 EMAIL_HOST_USER = os.environ.get(
-    'EMAIL_HOST_USER',
     'hanudigitalhub@gmail.com'
 )
 
 EMAIL_HOST_PASSWORD = os.environ.get(
-    'EMAIL_HOST_PASSWORD',
     'purz ysgs jhrn njgh'
 )
 
